9931e941c744a748bb86989e26859e505935e87c
Zenay83 <151439571+Zenay83@users.noreply.github.com>
2024-08-22 16:28:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let coinCount = 0;
 
 document.getElementById('bottle').addEventListener('click', function () {
     const bottle = document.getElementById('bottle');
-    bottle.style.transform = 'translateY(-100px) rotate(360deg)'; // Поворот бутылки
+    bottle.style.transform = 'translateY(-100px) rotate(360deg)'; 
 
     setTimeout(() => {
-        bottle.style.transform = 'translateY(0) rotate(0deg)'; // Вернуть в исходное положение
+        bottle.style.transform = 'translateY(0) rotate(0deg)'; 
         coinCount++;
         document.getElementById('coins').innerText = `Монеты: ${coinCount}`;
     }, 300);
